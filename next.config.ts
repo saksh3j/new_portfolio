ccfce3081878e6fbf1788cfebb6ffa7a875038cd
SAKSHAM JAIN <119560796+Insanely-capable@users.noreply.github.com>
2026-02-24 23:01:42 +0530

    chore: prepare project for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  /* Netlify handles everything automatically */
 };
 
 export default nextConfig;
